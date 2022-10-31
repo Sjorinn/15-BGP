850ddecdf3c0c84bba6acd912941249e6f9439e7
pde-bakk <pde-bakk@student.codam.nl>
2022-10-31 15:14:10 +0100

    created readme and installation script for GNS3 and Docker on Ubuntu

---
+++ P1/README.md
@@ -0,0 +1,17 @@
+# Part 1
+
+[Big ups to mr Ahmad Nadeem!](https://www.youtube.com/watch?v=D4nk5VSUelg)
+
+Step 1:
+Install the requirements using `install_gns3.sh`.
+
+Step 2:
+Build the docker images with `build_images.sh`.
+
+Step 3:
+* Create a new project in GNS3.
+* Add the Docker container with the docker image of frrouting.
+* Change the settings to enable BGPD, OSPFD and IS-IS.
+* Stop the container and save the container as a docker image (docker commit [CONTAINER_NAME] [IMAGE_NAME]).
+** We do this, so the changes in the container are persistent next time we re-launch the container.
+* Add the newly created docker image as a container in our GNS3 project.
---
+++ P1/install_gns3.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# https://docs.gns3.com/docs/getting-started/installation/linux/
+
+sudo add-apt-repository ppa:gns3/ppa
+sudo apt update -y                                
+sudo apt install -y gns3-gui gns3-server
+
+# For IOU support
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install gns3-iou
+
+# Install Docker CE
+sudo apt -y remove docker docker-engine docker.io
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install -y docker-ce
+
+# TODO: make this a loop
+sudo usermod -aG ubridge $USER
+sudo usermod -aG libvirt $USER
+sudo usermod -aG kvm $USER
+sudo usermod -aG wireshark $USER
+sudo usermod -aG docker $USER

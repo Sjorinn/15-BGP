ef1260326a52bc3d4d4ba24c2b2850f97f63cbae
pde-bakk <pde-bakk@student.codam.nl>
2022-11-02 12:01:14 +0100

    also added script to rename containers to match their hostnames, to remove confusion

--- P2/scripts/run_scripts.sh
+++ P2/scripts/run_scripts.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
-docker cp ./scripts/config_router1.sh router_pde-bakk-1:/root/
-docker exec -it router_pde-bakk-1 /root/config_router1.sh
+running_containers=$(docker ps -q)
+if [[ ! -z $running_containers ]]
+then
+  for i in ${running_containers[@]}
+  do
+	hostname=$(docker exec $i hostname)
+	echo "Renaming $i to $hostname"
+	docker rename "$i" "$hostname"
+  done
+else
+  echo "No running containers"
+  exit 1
+fi
 
-docker cp ./scripts/config_router2.sh router_pde-bakk-2:/root/
-docker exec -it router_pde-bakk-2 /root/config_router2.sh
+docker cp ./scripts/config_router1.sh router_${USER}-1:/root/
+docker exec -it router_${USER}-1 /root/config_router1.sh
 
-docker cp ./scripts/config_host1.sh host_pde-bakk-1:/root/
-docker exec -it host_pde-bakk-1 /root/config_host1.sh
+docker cp ./scripts/config_router2.sh router_${USER}-2:/root/
+docker exec -it router_${USER}-2 /root/config_router2.sh
 
-docker cp ./scripts/config_host2.sh host_pde-bakk-2:/root/
-docker exec -it host_pde-bakk-2 /root/config_host2.sh
+docker cp ./scripts/config_host1.sh host_${USER}-1:/root/
+docker exec -it host_${USER}-1 /root/config_host1.sh
+
+docker cp ./scripts/config_host2.sh host_${USER}-2:/root/
+docker exec -it host_${USER}-2 /root/config_host2.sh

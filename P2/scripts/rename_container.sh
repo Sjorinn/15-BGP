be6a614bad405894cca0ac62ca41938fa8b41b75
pde-bakk <pde-bakk@student.codam.nl>
2022-11-02 13:30:18 +0100

    removed script we're not using anymore

--- P2/scripts/rename_container.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CONTAINER_NAME=$1
-HOSTNAME=$(docker exec -it $CONTAINER_NAME hostname)
-echo "Let's change $CONTAINER_NAME to $HOSTNAME"
-docker rename $CONTAINER_NAME $HOSTNAME

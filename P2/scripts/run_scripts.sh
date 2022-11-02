bcb6429496b95fe17ebd08666a4fc6eca90a472e
Pierre CHAMBON-DUCASSE <pchambon@e1r3p1.clusters.42paris.fr>
2022-11-02 16:52:57 +0100

    commenting scripts

--- P2/scripts/run_scripts.sh
+++ P2/scripts/run_scripts.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 
+#Script to exec the config script inside each container
+
+#Every running containers into a var
 running_containers=$(docker ps -q)
+
+#If there is Running containers
 if [[ ! -z $running_containers ]]
 then
+# Then Rename them
   for i in ${running_containers[@]}
   do
 	hostname=$(docker exec $i hostname)
 	echo "Renaming $i to $hostname"
 	docker rename "$i" "$hostname"
   done
+# Else
 else
   echo "No running containers"
   exit 1
 fi
 
-docker cp ./scripts/config_router1.sh router_${USER}-1:/root/
-docker exec -it router_${USER}-1 /root/config_router1.sh
+# Then for each individual Docker, copy the corresponding script in it and exec it
 
-docker cp ./scripts/config_router2.sh router_${USER}-2:/root/
-docker exec -it router_${USER}-2 /root/config_router2.sh
+docker cp ./scripts/config_router1.sh router_pde-bakk-1:/root/
+docker exec -it router_pde-bakk-1 /root/config_router1.sh
 
-docker cp ./scripts/config_host1.sh host_${USER}-1:/root/
-docker exec -it host_${USER}-1 /root/config_host1.sh
+docker cp ./scripts/config_router2.sh router_pde-bakk-2:/root/
+docker exec -it router_pde-bakk-2 /root/config_router2.sh
 
-docker cp ./scripts/config_host2.sh host_${USER}-2:/root/
-docker exec -it host_${USER}-2 /root/config_host2.sh
+docker cp ./scripts/config_host1.sh host_pde-bakk-1:/root/
+docker exec -it host_pde-bakk-1 /root/config_host1.sh
+
+docker cp ./scripts/config_host2.sh host_pde-bakk-2:/root/
+docker exec -it host_pde-bakk-2 /root/config_host2.sh

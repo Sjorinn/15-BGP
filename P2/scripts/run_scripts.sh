abdbedb48d54a56c35fe9bd896c9fd0312a42c36
pde-bakk <pde-bakk@student.codam.nl>
2022-11-03 13:11:07 +0100

    updated script to check for same name

--- P2/scripts/run_scripts.sh
+++ P2/scripts/run_scripts.sh
@@ -3,7 +3,7 @@
 #Script to exec the config script inside each container
 
 #Every running containers into a var
-running_containers=$(docker ps -q)
+running_containers=$(docker ps --format "{{.Names}}")
 
 #If there is Running containers
 if [[ ! -z $running_containers ]]
@@ -12,16 +12,17 @@ then
   for i in ${running_containers[@]}
   do
 	hostname=$(docker exec $i hostname)
-	echo "Renaming $i to $hostname"
-	docker rename "$i" "$hostname"
+	if [[ "$i" != "$hostname" ]]; then
+		echo "Renaming $i to $hostname"
+		docker rename "$i" "$hostname"
+	fi
   done
-# Else
 else
   echo "No running containers"
   exit 1
 fi
 
-# Then for each individual Docker, copy the corresponding script in it and exec it
+# Then for each individual Docker container, copy the corresponding script in it and exec it
 
 docker cp ./scripts/config_router1.sh router_pde-bakk-1:/root/
 docker exec -it router_pde-bakk-1 /root/config_router1.sh
